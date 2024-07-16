4e413f4978e3443ee80e681865c2bb2c49451d0d
Emogy-reunion <mv7786986@gmail.com>
2024-07-16 16:46:05 +0300

    create: first commit

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+    Python function that lists all documents in a collection:
+    Prototype: def list_all(mongo_collection):
+    Return an empty list if no document in the collection
+    mongo_collection will be the pymongo collection object
+'''
+
+def list_all(mongo_collection):
+    """Return list of all dococuments in collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
+
