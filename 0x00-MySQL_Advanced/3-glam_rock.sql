1a4afdb919a067f55f79b6db121b7486f1939079
Emogy-reunion <mv7786986@gmail.com>
2024-08-31 08:30:40 +0300

    minor changes

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
-SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+-- A SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
     FROM metal_bands
-    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    WHERE style LIKE '%Glam rock%'
     ORDER BY lifespan DESC;
