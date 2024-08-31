b69e827649c916b6f4cbabb3702e6d0a0b47042c
Emogy-reunion <mv7786986@gmail.com>
2024-08-31 08:38:04 +0300

    first commit

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-'''
-Task 12's module.
+'''Task 15's module.
 '''
 from pymongo import MongoClient
 
@@ -21,12 +20,37 @@ def print_nginx_request_logs(nginx_collection):
     print('{} status check'.format(status_checks_count))
 
 
+def print_top_ips(server_collection):
+    '''
+    Prints statistics about the top 10 HTTP IPs in a collection.
+    '''
+    print('IPs:')
+    request_logs = server_collection.aggregate(
+        [
+            {
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
+            },
+            {
+                '$sort': {'totalRequests': -1}
+            },
+            {
+                '$limit': 10
+            },
+        ]
+    )
+    for request_log in request_logs:
+        ip = request_log['_id']
+        ip_requests_count = request_log['totalRequests']
+        print('\t{}: {}'.format(ip, ip_requests_count))
+
+
 def run():
     '''
     Provides some stats about Nginx logs stored in MongoDB.
     '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)
+    print_top_ips(client.logs.nginx)
 
 
 if __name__ == '__main__':
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-'''
-    Python function that returns the list of school having a specific topic:
-    Prototype: def schools_by_topic(mongo_collection, topic):
-    mongo_collection will be the pymongo collection object
-    topic (string) will be topic searched
+'''Task 11's module.
 '''
 
+
 def schools_by_topic(mongo_collection, topic):
-    """returns the list of school having a specific topic"""
-    return mongo_collection.find({"topics": topic})
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
